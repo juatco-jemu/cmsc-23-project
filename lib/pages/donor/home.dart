9e7b61e2f81995367344dc6d35d833b28a58d6f2
jemooooo <jjuatco@up.edu.ph>
2024-05-15 14:31:51 +0800

    feat: initial draft of donor homepage

--- lib/pages/donor/home.dart
+++ lib/pages/donor/home.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:donation_system/components/appbar.dart';
 import 'package:donation_system/mock/mock_donor.dart';
 import 'package:donation_system/model/model_user.dart';
+import 'package:donation_system/pages/org_list.dart';
 import 'package:donation_system/theme/widget_designs.dart';
 import 'package:flutter/material.dart';
 
@@ -16,29 +18,47 @@ class _DonorHomepageState extends State<DonorHomepage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-          decoration: CustomWidgetDesigns.gradientBackground(),
-          child: Column(
-            children: [
-              Text("Hello, ${donor.name}"),
-              const Text("A little help goes a long way!"),
-              viewAll,
-              carouselSlider,
-              const Text("Your recent Donations"),
-              viewAll,
-              carouselSlider,
-            ],
-          )),
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Elbi Donation System'),
+      body: SingleChildScrollView(
+        child: Container(
+            decoration: CustomWidgetDesigns.gradientBackground(),
+            child: Column(
+              children: [
+                spacer,
+                header,
+                subHeader("Featured Organizations"),
+                carouselSlider,
+                spacer,
+                subHeader("Recent Donations"),
+                carouselSlider,
+                spacer,
+              ],
+            )),
+      ),
     );
   }
 
-  Widget get viewAll => const Padding(
-        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+  Widget get header => Column(
+        children: [
+          Text("Hello, ${donor.name}",
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          const Text("A little help goes a long way!"),
+        ],
+      );
+
+  Widget subHeader(title) => Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text("View all"),
+            Text(title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => const DonationList()));
+                },
+                child: const Text("View All")),
           ],
         ),
       );
@@ -64,4 +84,6 @@ class _DonorHomepageState extends State<DonorHomepage> {
         enableInfiniteScroll: false,
         scrollDirection: Axis.horizontal,
       ));
+
+  Widget get spacer => const SizedBox(height: 30);
 }
