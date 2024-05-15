f2eac9f913c8f7b619b0467b0b1c700c2010cb53
jemooooo <jjuatco@up.edu.ph>
2024-05-15 10:10:35 +0800

    feat: added bottom navbar widget; moved homepage to new donor folder

---
+++ lib/pages/donor/homepage.dart
@@ -0,0 +1,60 @@
+import 'package:donation_system/pages/admin/admin_homepage.dart';
+import 'package:donation_system/pages/org_list.dart';
+import 'package:donation_system/pages/signin_page.dart';
+import 'package:donation_system/pages/signup_page.dart';
+import 'package:donation_system/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../components/appbar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    const DonationList(),
+    const SignInPage(),
+    const SignUpPage(),
+    const AdminPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: const CustomAppBar(
+          title: 'Elbi Donation System',
+        ),
+        bottomNavigationBar: botNavBar,
+        body: Center(child: _pages.elementAt(_selectedIndex)));
+  }
+
+  BottomNavigationBar get botNavBar => BottomNavigationBar(
+          backgroundColor: AppColors.tiffanyBlue,
+          onTap: _onItemTapped,
+          currentIndex: _selectedIndex,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.approval),
+              label: 'Donate',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'You',
+            ),
+          ]);
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
