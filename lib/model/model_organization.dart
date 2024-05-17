90d18d6489bc7d6f254b04ba52b957d680898d9e
jemooooo <jjuatco@up.edu.ph>
2024-05-17 16:45:07 +0800

    chore: added list of donation drives to organization model

--- lib/model/model_organization.dart
+++ lib/model/model_organization.dart
@@ -1,13 +1,17 @@
+import 'package:donation_system/model/model_donation_drive.dart';
+
 class Organization {
   String name;
   List<String> proofsOfLegitimacy;
   String status;
   String about;
+  List<DonationDrive> donationDrives = [];
 
   Organization({
     required this.name,
     required this.proofsOfLegitimacy,
     required this.status,
     required this.about,
+    required this.donationDrives,
   });
 }
