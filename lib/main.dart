89062d7675ea8858d681e21a46e88b3b3a66d40a
jemooooo <jjuatco@up.edu.ph>
2024-05-17 16:23:08 +0800

    feat: added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:donation_system/pages/donor/homepage.dart';
+import 'package:donation_system/pages/donor/list_of_user_donations_page.dart';
+import 'package:donation_system/pages/org_list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +20,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      // home: const HomePage(),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const HomePage(),
+        "/org-list": (context) => const OrganizationsList(),
+        "/user-donation-list": (context) => const UserDonationsList(),
+      },
     );
   }
 }
