ad47581b7acf49068fa8a5a533debe238a68b8e8
jemooooo <jjuatco@up.edu.ph>
2024-05-17 16:30:48 +0800

    chore: added HopeHaven app colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -11,4 +11,11 @@ class AppColors {
   static const carribeanCurrent = Color(0xff006D77);
   static const paleDogwood = Color(0xffffddd2);
   static const atomicTangerine = Color(0xffe29578);
+
+  //HopeHaven app colors
+  static const appWhiteYellow = Color.fromARGB(255, 255, 255, 246);
+  static const yellow01 = Color(0xffffe094);
+  static const yellow02 = Color(0xfff6c54a);
+  static const yellow03 = Color(0xffffc331);
+  static const darkYellow01 = Color(0xff334633);
 }
