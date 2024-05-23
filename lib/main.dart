0f603c3662d56fbc82b7dc7a09a0639d9475ddb1
jemooooo <jjuatco@up.edu.ph>
2024-05-23 09:37:03 +0800

    chore: changed initial route for ui building

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: "/sign-in",
+      initialRoute: "/",
       routes: {
         "/sign-in": (context) => const SignInPage(),
         "/sign-up-donor": (context) => const SignUpPage(),
