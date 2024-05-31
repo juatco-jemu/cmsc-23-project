076bfa2593cd9fd493e94a4cac4093630dd85280
jemooooo <jjuatco@up.edu.ph>
2024-05-31 15:02:41 +0800

    chore: updated qr dialog UI

--- lib/components/qrCode.dart
+++ lib/components/qrCode.dart
@@ -1,4 +1,5 @@
 import 'package:donation_system/theme/colors.dart';
+import 'package:donation_system/theme/widget_designs.dart';
 import 'package:flutter/material.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
 
@@ -13,20 +14,53 @@ class _GenerateQRCodeState extends State<GenerateQRCode> {
   String? qrData;
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-        child: const Text(
-          "Generate QR Code",
-          style: TextStyle(color: AppColors.yellow03),
-        ),
-        onPressed: () {
-          setState(() {
-            qrData = DateTime.now().toString();
-          });
-          showDialog(
-              context: (context),
-              builder: (context) => Dialog(
-                    child: (qrData != null) ? PrettyQrView.data(data: qrData!) : Container(),
-                  ));
-        });
+    return qrData != null
+        ? TextButton(
+            child: const Text(
+              "View QR Code",
+              style: TextStyle(color: AppColors.yellow03),
+            ),
+            onPressed: () {
+              showDialog(
+                  context: (context),
+                  builder: (context) => Dialog(
+                        child: qrDialog(qrData),
+                      ));
+            })
+        : TextButton(
+            child: const Text(
+              "Generate QR Code",
+              style: TextStyle(color: AppColors.yellow03),
+            ),
+            onPressed: () {
+              setState(() {
+                qrData = DateTime.now().toString();
+              });
+              showDialog(
+                  context: (context),
+                  builder: (context) => Dialog(
+                        child: qrDialog(qrData),
+                      ));
+            });
+  }
+
+  Widget qrDialog(qrData) {
+    return Container(
+        width: 150,
+        padding: const EdgeInsets.all(20),
+        decoration: CustomWidgetDesigns.customContainer(),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            PrettyQrView.data(data: qrData!),
+            spacer(20),
+            const Text("Here is your QR Code. Tap anywhere outside the box to close this dialog.",
+                style: TextStyle(fontSize: 20)),
+          ],
+        ));
+  }
+
+  Widget spacer(double height) {
+    return SizedBox(height: height);
   }
 }
