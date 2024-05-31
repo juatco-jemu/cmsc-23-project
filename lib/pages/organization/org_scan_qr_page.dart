1da7ed6d1d7d16dfe90af92d9dce76e1cc09c203
jemooooo <jjuatco@up.edu.ph>
2024-05-31 16:59:35 +0800

    fix: scanner can now properly scan qr code

--- lib/pages/organization/org_scan_qr_page.dart
+++ lib/pages/organization/org_scan_qr_page.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:donation_system/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -14,6 +15,7 @@ class OrgScanQRCodePage extends StatefulWidget {
 }
 
 class _OrgScanQRCodePageState extends State<OrgScanQRCodePage> {
+  late Size screen = MediaQuery.of(context).size;
   @override
   void initState() {
     super.initState();
@@ -27,35 +29,54 @@ class _OrgScanQRCodePageState extends State<OrgScanQRCodePage> {
           title: "Scan QR Code",
         ),
         body: Center(
-          child: Container(
-            padding: const EdgeInsets.only(bottom: 300),
-            child: MobileScanner(
-              controller: MobileScannerController(
-                  detectionSpeed: DetectionSpeed.noDuplicates, returnImage: true),
-              onDetect: (capture) {
-                final List<Barcode> barcodes = capture.barcodes;
-                final Uint8List? image = capture.image;
-                for (final barcode in barcodes) {
-                  print("Barcode: ${barcode.rawValue}");
-                }
+          child: Column(
+            children: [
+              SizedBox(
+                width: screen.width,
+                height: screen.width,
+                child: MobileScanner(
+                  controller: MobileScannerController(
+                      detectionSpeed: DetectionSpeed.noDuplicates, returnImage: true),
+                  onDetect: (capture) {
+                    final List<Barcode> barcodes = capture.barcodes;
+                    final Uint8List? image = capture.image;
+                    for (final barcode in barcodes) {
+                      print("Barcode: ${barcode.rawValue}");
+                    }
 
-                if (image != null) {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text(barcodes.first.rawValue ?? "No QR Code Found"),
-                          content: Column(
-                            children: [
-                              Image.memory(image),
-                              const Text("Successfully scanned QR Code!"),
-                            ],
-                          ),
-                        );
-                      });
-                }
-              },
-            ),
+                    if (image != null) {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text(barcodes.first.rawValue ?? "No QR Code Found"),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Image.memory(image),
+                                  const Text(
+                                      "Successfully Confirmed donation drop-off! Tap outside this dialog to close.",
+                                      style: TextStyle(fontSize: 20)),
+                                ],
+                              ),
+                            );
+                          });
+                    }
+                  },
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text("Scan the QR Code to confirm the drop-off donation.",
+                    style: TextStyle(fontSize: 20)),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel",
+                      style: TextStyle(color: AppColors.yellow03, fontSize: 20)))
+            ],
           ),
         ));
   }
